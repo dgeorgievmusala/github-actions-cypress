10d14906f1de8ea1077ea5056561a1ef974560b9
Diyan G. Georgiev <diyan.georgiev@MUSALA.COM>
2025-05-09 15:37:51 +0300

    Add GItHUb actions file for more triggers with chrone

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "24349fbb-a3c4-47cd-bb64-4e45a18cc762",
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     charts: true,
@@ -8,6 +9,7 @@ module.exports = defineConfig({
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
+    videoOnFailOnly: true,
   },
   e2e: {
     setupNodeEvents(on, config) {
