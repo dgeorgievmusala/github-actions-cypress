3a6136cfcc9c6e5f9f4208a1593e33bfaac66423
Diyan G. Georgiev <diyan.georgiev@MUSALA.COM>
2025-05-12 09:21:53 +0300

    fail test to check in  github Actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "24349fbb-a3c4-47cd-bb64-4e45a18cc762",
+  //projectId: "24349fbb-a3c4-47cd-bb64-4e45a18cc762",
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     charts: true,
@@ -12,6 +12,7 @@ module.exports = defineConfig({
     videoOnFailOnly: true,
   },
   e2e: {
+    baseUrl: "https://example.cypress.io",
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
     },
